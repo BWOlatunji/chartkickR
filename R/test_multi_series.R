a27e336595d0629cadd8f372a486b0b2a97c08a0
BWOlatunji <bilqiiswunmi@gmail.com>
2023-09-13 10:24:28 +0100

    updated utils.R file for multiple series data

--- R/test_multi_series.R
+++ R/test_multi_series.R
@@ -1,13 +1,21 @@
-#
-# library(tidyverse)
-# library(chartkickR)
-#
-# multi_series_tbl <- read_csv("inst/examples/sample_data/multi_series_line.csv")
-#
-#
-# chartkickR(data = multi_series_tbl, x=task_name, y=values,
-#            group=dates, type = "LineChart",curve = TRUE)
-#
-# test_func_data <- process_data(multi_series_tbl)
+# # #
+library(tidyverse)
+library(chartkickR)
+
+multi_series_tbl <- read_csv("inst/examples/sample_data/multi_series_line.csv")
+
+aa <- process_data(multi_series_tbl,x=dates,y=values, group=task_name)
+
+jsonlite::toJSON(get_unique(multi_series_tbl,x=dates,y=values, group=task_name))
 
 
+chartkickR(data = multi_series_tbl, x=dates, y=values,
+           group=task_name, type = "LineChart",curve = TRUE)
+
+chartkickR(data = multi_series_tbl, x="dates", y="values",
+           group="task_name", type = "LineChart",curve = TRUE)
+
+# test_func_data <- process_data(multi_series_tbl,x=dates, y=values,
+#                                group=task_name)
+#
+# toJSON(test_func_data)
--- R/utils.R
+++ R/utils.R
@@ -1,33 +1,24 @@
 #' Preprocess data frame
 #'
-#' @param data A data frame
-#' @param x string value of column name containing values on the x-axis
-#' @param y string value of column name containing values on the y-axis
-#' @param group string value of column name for grouping
+#' @param df A data frame
+#' @param x_col string value of column name containing values on the x-axis
+#' @param y_col string value of column name containing values on the y-axis
+#' @param group_col string value of column name for grouping
 #'
 #' @import assertthat dplyr magrittr tidyr
 #'
-#' @importFrom rlang enquo
-#' @importFrom rlang expr
-#' @importFrom rlang eval_tidy
 #' @importFrom stats setNames
-process_data <- function(data, x, y, group) {
-  x <- enquo(x)
-  y <- enquo(y)
-  group <- enquo(group)
-  group_vals_expr <- expr(pull(data, !!group))
-  group_vals <- eval_tidy(group_vals_expr)
+process_data <- function(df, x_col, y_col, group_col) {
 
-  # Split the data frame by task_name and create a list of JSON objects
-  json_list <- lapply(split(data, group_vals), function(sub_df) {
-    name <- unique(pull(sub_df, !!group))
-    data <- setNames(as.list(pull(sub_df, !!y)), pull(sub_df, !!x))
-    list(name = name, data = data)
-  })
+  json_list <- df %>%
+    group_by({{group}}) %>%
+    summarise(data = list(setNames({{y_col}}, {{x_col}}))) %>%
+    ungroup() %>%
+    mutate(name = {{group_col}}, data = map(data, ~as.list(.x))) %>%
+    select(-{{group}})
 
   return(json_list)
 }
-
 
 #' @importFrom magrittr %>%
 #' @export

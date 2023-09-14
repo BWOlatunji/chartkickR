9d08946add12a692923056f42c60f692369e3278
BWOlatunji <bilqiiswunmi@gmail.com>
2023-09-14 19:14:01 +0100

    updated utils.R

--- R/utils.R
+++ R/utils.R
@@ -25,38 +25,3 @@ process_data <- function(df, group_col, x_col, y_col) {
   return(data_list)
 }
 
-# process_data <- function(df, group_col, x_col, y_col) {
-#   df  |>
-#     dplyr::summarise(data = list(setNames(as.list({{ y_col }}),
-#                                           {{ x_col }})),
-#                      .by = {{ group_col }})  |>
-#     dplyr::mutate(data = purrr::map(data, as.list))  |>
-#     dplyr::select({{ group_col }}, data)
-# }
-
-
-
-# process_data <- function(df, x_col, y_col, group_col) {
-#
-#   json_list <- df |>
-#     dplyr::summarise(data = list(setNames({{ y_col }}, {{ x_col }})), .by = {{ group_col }}) |>
-#     dplyr::mutate(name = {{ group_col }}, data = purrr::map(data, \(x) as.list(x))) |>
-#     dplyr::select(-{{ group_col }})
-#
-#   return(json_list)
-# }
-
-
-# process_data <- function(df, x_col, y_col, group_col) {
-#
-#   json_list <- df %>%
-#     dplyr::group_by({{group_col}}) %>%
-#     summarise(data = list(setNames({{y_col}}, {{x_col}}))) %>%
-#     ungroup() %>%
-#     mutate(name = {{group_col}}, data = map(data, ~as.list(.x))) %>%
-#     select(-{{group}})
-#
-#   return(json_list)
-# }
-
-

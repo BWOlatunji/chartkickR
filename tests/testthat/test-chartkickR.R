d3b140ed9f97c218fefd32ab8caf146a2e47b099
BWOlatunji <bilqiiswunmi@gmail.com>
2023-10-21 07:31:46 +0100

    added test

---
+++ tests/testthat/test-chartkickR.R
@@ -0,0 +1,25 @@
+test_that("chartkickR function tests", {
+
+  # Create some sample data
+  data <- data.frame(
+    x = c(1, 2, 3, 4, 5),
+    y = c(10, 20, 30, 40, 50),
+    group = c("A", "A", "B", "B", "C")
+  )
+
+  # Test that chartkickR returns a htmlwidget object
+  expect_type(chartkickR(data, x = "x", y = "y", group = "group", type = "LineChart"), "list")
+
+  # Test that chartkickR throws an error if data is not a data.frame
+  expect_error(chartkickR(list(x = 1, y = 2), x = "x", y = "y", type = "PieChart"))
+
+  # Test that chartkickR throws an error if type is not a valid chart type
+  expect_error(chartkickR(data, x = "x", y = "y", type = "InvalidChart"))
+
+  # Test that chartkickR passes the options to the widget
+  expect_equal(chartkickR(data, x = "x", y = "y", type = "BarChart", colors = c("red", "blue"))$x$options$colors, c("red", "blue"))
+
+})
+
+
+
